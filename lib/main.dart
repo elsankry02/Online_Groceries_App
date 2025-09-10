4dfb6ba7cb0ba3c974ffada6d5861a71549da6c8
elsankry02 <githup800900@gmail.com>
2025-09-10 13:13:33 +0300

    feat: Initialize macOS and web project structure for online groceries app
    - Added macOS project files including AppIcon, MainMenu.xib, and Info.plist.
    - Configured application settings in AppInfo.xcconfig and added build configurations for Debug and Release.
    - Implemented MainFlutterWindow for macOS and added entitlements for sandboxing.
    - Created initial test file for RunnerTests.
    - Set up pubspec.yaml with dependencies and versioning for Flutter project.
    - Added web files including index.html, manifest.json, and favicon icons for web app.

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const OnlineGroceriesApp());
+}
+
+class OnlineGroceriesApp extends StatelessWidget {
+  const OnlineGroceriesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
